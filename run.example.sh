e0a19ec9071bf82d7be2afb2bed997918a997e1d
Exaltead <aku.pohjola@gmail.com>
2019-04-28 16:31:09 +0300

    Update cron-scritp to have env.

--- run.example.sh
+++ run.example.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+PID=$(pgrep -o "cinnamon-sess|gnome-sess|mate-sess")
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+
 cd CHANGEME
 venv/bin/python main.py
